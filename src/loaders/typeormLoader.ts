b293166b737df108f1a3485ed5fbe47ecbb97e52
Juan José González Rodríguez <juanjosealosnero@gmail.com>
2020-08-15 12:53:38 +0200

    heroku: trycatch

--- src/loaders/typeormLoader.ts
+++ src/loaders/typeormLoader.ts
@@ -1,32 +1,49 @@
 import { MicroframeworkLoader, MicroframeworkSettings } from 'microframework-w3tec';
-import { createConnection } from 'typeorm';
+import { createConnection, getConnectionOptions, ConnectionOptions } from 'typeorm';
 
 import { env } from '@src/env';
 
 export const typeormLoader: MicroframeworkLoader = async (settings: MicroframeworkSettings | undefined) => {
 
-    const connectionOptions = {
-        url: env.db.url,
-        type: env.db.type as any,
-        host: env.db.host,
-        port: env.db.port,
-        username: env.db.username,
-        password: env.db.password,
-        database: env.db.database,
-        synchronize: env.db.synchronize,
-        logging: env.db.logging,
-        entities: env.app.dirs.entities,
-        migrations: env.app.dirs.migrations,
-        ssl: {
-            rejectUnauthorized: false,
-        },
+    const connectionOptions = async () => {
+        // tslint:disable-next-line: no-shadowed-variable
+        let connectionOptions: ConnectionOptions;
+        connectionOptions = {
+            type: env.db.type as any,
+            host: env.db.host,
+            port: env.db.port,
+            username: env.db.username,
+            password: env.db.password,
+            database: env.db.database,
+            synchronize: env.db.synchronize,
+            logging: env.db.logging,
+            entities: env.app.dirs.entities,
+            migrations: env.app.dirs.migrations,
+            ssl: {
+                rejectUnauthorized: false,
+            },
+        };
+
+        if (process.env.DATABASE_URL || env.db.url) {
+            Object.assign(connectionOptions, { url: process.env.DATABASE_URL || env.db.url });
+        } else {
+            // gets your default configuration
+            // you could get a specific config by name getConnectionOptions('production')
+            // or getConnectionOptions(process.env.NODE_ENV)
+            connectionOptions = await getConnectionOptions();
+         }
+
+        return connectionOptions;
     };
 
     console.log('work');
-
-    const connection = await createConnection(connectionOptions);
-
-    console.log('still working');
+    let connection;
+    try {
+        connection = await createConnection(await connectionOptions());
+        console.log('still working');
+    } catch (error) {
+        console.log(error);
+    }
 
     settings.setData('connection', connection);
     settings.onShutdown(() => connection.close());
